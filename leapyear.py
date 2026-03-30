4b9d4f2c5304a405a0c07f37f44038d6aee22aae
Nhi <ncelios@Nhis-MacBook-Pro.local>
2026-03-30 21:04:02 +1100

    hi

--- leapyear.py
+++ leapyear.py
@@ -1,15 +1,29 @@
-def is_leap(year):
-    if year % 4 == 0:
-        if year % 100 ==0:
-            if year % 400 ==0:
-                leap = True
-            else: 
-                leap = False
-        else:
-            leap = True
+# def is_leap(year):
+#     if year % 4 == 0:
+#         if year % 100 ==0:
+#             if year % 400 ==0:
+#                 leap = True
+#             else: 
+#                 leap = False
+#         else:
+#             leap = True
             
+#     else:
+#         leap = False
+#     return leap
+
+# year = int(input())
+
+def is_leap(year):
+    if year % 400 == 0:
+        leap = True
+    elif year % 100 == 0:
+        leap = False
+    elif year % 4 == 0:
+        leap = True
     else:
         leap = False
     return leap
-
+       
 year = int(input())
+print(is_leap(year))
